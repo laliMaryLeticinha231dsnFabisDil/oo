7671ace7b9acda300a3fc25f6a805297bdb5dd8f
laliMaryLeticinha231dsnFabisDil <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2023-09-28 11:56:30 -0300

    kk

---
+++ 02/main.js
@@ -0,0 +1,36 @@
+class Calculadora {
+    constructor() {
+        //inicializando o visor com valor 0
+        this.valor
+    }
+    //método para adicionar número
+    adicionarNumero(numero) {
+        if (this.valor === '0') {
+            //se o valor atual for 0 vai substituir
+            this.valor = numero
+
+        } else {
+            //senãoadicionar o numero no final do valor atuaç
+            this.valor += numero
+        }
+        //atualiza o visor
+        this.atualizarVisor()
+
+        adicionarOperacao(operacao) {
+            if (
+                this.valor.slice(-1) === '+' ||
+                this.valor.slice(-1) === '-' ||
+                this.valor.slice(-1) === '*' ||
+                this.valor.slice(-1) === '/'
+            ) {
+                //se o ultimo caractere do valor atual for uma operação substitui essa operação nova pela nova operação
+                this.valor = this.valor.slice(0, -1) + operacao
+            } else {
+                //senão adiciona a operação
+                this.valor += operacao
+            }
+            //atualiza o visor
+            this.atualizarVisor()
+        }
+    }
+}
